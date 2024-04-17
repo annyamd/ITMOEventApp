1948e833ba35cfa9ee58a68d96077887775257ec
annyamd <miann9920@gmail.com>
2024-04-17 11:19:24 +0300

    Add place screen: xml and impl

--- app/src/main/java/org/itmo/itmoevent/view/MainActivity.kt
+++ app/src/main/java/org/itmo/itmoevent/view/MainActivity.kt
@@ -17,6 +17,7 @@ import org.itmo.itmoevent.view.fragments.ActivityFragment
 import org.itmo.itmoevent.view.fragments.EventFragment
 import org.itmo.itmoevent.view.fragments.EventSectionFragment
 import org.itmo.itmoevent.view.fragments.ManagementSectionFragment
+import org.itmo.itmoevent.view.fragments.PlaceFragment
 import org.itmo.itmoevent.view.fragments.ProfileSectionFragment
 import org.itmo.itmoevent.view.fragments.TaskSectionFragment
 import org.itmo.itmoevent.viewmodel.MainViewModel
@@ -97,6 +98,16 @@ class MainActivity : AppCompatActivity() {
                         supportFragmentManager.beginTransaction()
                             .setReorderingAllowed(true)
                             .replace<ActivityFragment>(R.id.main_fragment_container, args = argBundle)
+                            .addToBackStack(BACK_STACK_DETAILS_TAG)
+                            .commit()
+                    }
+
+                    mainViewModel.placeId.observe(this@MainActivity) { id ->
+                        val argBundle =
+                            bundleOf(PlaceFragment.PLACE_ID_ARG to id)
+                        supportFragmentManager.beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace<PlaceFragment>(R.id.main_fragment_container, args = argBundle)
                             .addToBackStack(BACK_STACK_DETAILS_TAG)
                             .commit()
                     }
