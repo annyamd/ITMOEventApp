ea88f7e651fd60eca395abbd7163360239079e6c
annyamd <miann9920@gmail.com>
2024-04-19 10:48:33 +0300

    Add token auth to EventNetworkService, update api calls due to changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
 }
